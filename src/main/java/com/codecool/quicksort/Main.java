2ffa5ce0b5874223df20e86bbb4b2b7e5ae776a8
Zoli <zoltan.konradi@gmail.com>
2019-05-15 14:19:09 +0200

    Quicksort implemented

--- src/main/java/com/codecool/quicksort/Main.java
+++ src/main/java/com/codecool/quicksort/Main.java
@@ -1,9 +1,19 @@
 package com.codecool.quicksort;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         // Use the tests to check your code!
+        QuickSort quickSort = new QuickSort();
+
+        List<Integer> toSort = Arrays.asList(5, 3, 1, 5, 9, 3, 2, 10, 6);
+        quickSort.sort(toSort);
+        for (int i: toSort) {
+            System.out.println(i + " ");
+        }
     }
 
 }
--- src/main/java/com/codecool/quicksort/QuickSort.java
+++ src/main/java/com/codecool/quicksort/QuickSort.java
@@ -9,6 +9,29 @@ public class QuickSort {
      * @param toSort the List to sort. Throws an error if its null
      */
     public void sort(List<Integer> toSort) {
+        if (toSort == null) throw new IllegalArgumentException();
+        if (toSort.size() <= 1) return;
 
+        quickSort(toSort,0, toSort.size() - 1);
+    }
+
+    private void quickSort(List<Integer> toSort, int left, int right) {
+        int i = left;
+        int j = right;
+        int pivot = toSort.get(left + (right - left) / 2);
+
+        while (i <= j) {
+            while (toSort.get(i) < pivot) i++;
+            while (toSort.get(j) > pivot) j--;
+            if (i <= j) {
+                int temp = toSort.get(i);
+                toSort.set(i, toSort.get(j));
+                toSort.set(j, temp);
+                i++;
+                j--;
+            }
+        }
+        if (left < j) { quickSort(toSort, left, j); }
+        if (i < right) { quickSort(toSort, i, right); }
     }
 }
